2fec4a3c3530897e34c1f2925df1f8dac4d65ff3
takeshisakuma <saude0518@gmail.com>
2021-04-04 21:42:02 +0900

    Updates

---
+++ precache-manifest.dda5d8aeeb7179397e10c45c6675aefb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "813e3402bdaa38d8d34bcfe597dac32f",
+    "url": "/movement/index.html"
+  },
+  {
+    "revision": "68306d6ae49aeb9074d8",
+    "url": "/movement/static/css/main.26efc2f6.chunk.css"
+  },
+  {
+    "revision": "e2c935fa69afaed93740",
+    "url": "/movement/static/js/2.f7678c71.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/movement/static/js/2.f7678c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68306d6ae49aeb9074d8",
+    "url": "/movement/static/js/main.cce3265b.chunk.js"
+  },
+  {
+    "revision": "5a731e95c5cd55a1642e",
+    "url": "/movement/static/js/runtime-main.7d462874.js"
+  }
+]);
